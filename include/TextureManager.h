b7ecdc077d6fcdef02f9ce50644c206e23d32441
tolerance1 <tolerance1@users.noreply.github.com>
2014-04-07 19:37:10 +0300

    Removed two now obsolete functions

--- include/TextureManager.h
+++ include/TextureManager.h
@@ -12,19 +12,13 @@ class TextureManager
         ~TextureManager();
 
         bool createTexture(std::string fileName, std::string textureID,
-                         SDL_Renderer* pRenderer);
-        void drawStatic(std::string id, int destX, int destY,
-                        int width, int height,
-                        SDL_Renderer* pRenderer,
-                        SDL_RendererFlip flip = SDL_FLIP_NONE);
-        void drawAnimated(std::string id, int destX, int destY,
-                          int width, int height,
-                          int currentRow, int currentFrame,
-                          SDL_Renderer* pRenderer,
-                          double rotationAngle,
-                          SDL_RendererFlip flip);
-        void DestroyTextures();
-
+                           SDL_Renderer* pRenderer);
+        void drawTexture(std::string id, int destX, int destY,
+                         int width, int height,
+                         int currentRow, int currentFrame,
+                         SDL_Renderer* pRenderer,
+                         double rotationAngle,
+                         SDL_RendererFlip flip);
 
         static TextureManager* getpTextureManager();//create a single instance
 
