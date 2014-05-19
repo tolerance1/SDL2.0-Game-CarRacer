2150a4b95309d7ce30049059c2e852292bc44c49
tolerance1 <tolerance1@users.noreply.github.com>
2014-05-20 00:06:40 +0300

    Implemented a fixed frame rate game main loop

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ using std::endl;
 #include <stdio.h>
 //END REMOVE
 
+const size_t FPS = 60;
+const size_t DELAY_TIME = 1000 / FPS;
+
 int main(int argc, char* argv[])
 {
     //REMOVE THIS WHEN RELEASING THE GAME
@@ -16,6 +19,7 @@ int main(int argc, char* argv[])
     freopen("CON", "w", stdout);
     //END REMOVE
 
+    size_t frameStartTime = 0, frameElapsedTime = 0;
 
     //create the game object
     if (Game::getpGame()->init() )
@@ -24,11 +28,22 @@ int main(int argc, char* argv[])
 
         while (Game::getpGame()->getbRunning() )
             {
+                frameStartTime = SDL_GetTicks();
+
                 Game::getpGame()->getInput();
                 Game::getpGame()->update();
                 Game::getpGame()->render();
 
-                SDL_Delay(10);
+                frameElapsedTime = SDL_GetTicks() - frameStartTime;
+
+                if (frameElapsedTime < DELAY_TIME)
+                {
+                    SDL_Delay(DELAY_TIME - frameElapsedTime);
+                }
+                else
+                {
+                    cout << "Loop iteration, " << frameElapsedTime << " ms, exceeded frame time, " << DELAY_TIME << " ms!" << endl;
+                }
             }
     }
     else
