793a5183996e0675cdd8f63ec8d669936444c0cb
tolerance1 <tolerance1@users.noreply.github.com>
2014-08-04 20:05:44 +0300

    Changed getStateID() function return type to a reference

--- include/GameStateABC.h
+++ include/GameStateABC.h
@@ -15,7 +15,7 @@ class GameStateABC
         virtual bool onEnter() = 0;
         virtual bool onExit() = 0;
 
-        virtual std::string getStateID() const = 0;
+        virtual const std::string& getStateID() const = 0;
 };
 
 #endif // GAMESTATEABC_H
