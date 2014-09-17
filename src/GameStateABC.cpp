a285a03e0da62cbb8c3181fd2d1b1943417262ca
tolerance1 <tolerance1@users.noreply.github.com>
2014-09-17 21:15:32 +0300

    Amended the class destructor to release the state's game objects. Implemented an onExit() function to release the state's textures.

--- src/GameStateABC.cpp
+++ src/GameStateABC.cpp
@@ -1,5 +1,7 @@
 #include "GameStateABC.h"
 
+#include "TextureManager.h"
+
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -12,4 +14,21 @@ GameStateABC::GameStateABC()
 GameStateABC::~GameStateABC()
 {
     cout << " 10 D GameStateABC" << endl;
+
+    //release game objects
+    for(size_t Index = 0; Index != gameObjects.size(); ++Index)
+    {
+        delete gameObjects[Index];
+    }
+}
+
+bool GameStateABC::onExit()
+{
+    //release the state's textures
+    for(size_t Index = 0; Index != gameObjects.size(); ++Index)
+    {
+        TextureManager::getpTextureManager()->destroyTexture(textureIDs[Index]);
+    }
+
+    return true;
 }
