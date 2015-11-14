1c136c23f030ae1c63f2d7266416a26207b5a307
tolerance1 <tolerance1@users.noreply.github.com>
2015-11-14 18:33:34 +0200

    Added a variable to distinguish between a static and an animated tileset

--- include/Tileset.h
+++ include/Tileset.h
@@ -16,6 +16,8 @@ class Tileset
         int height;//tileset image height in pixels
         int numColumns;//used to find a tile position
         std::string name;
+
+        int animSpeed {0};//for an animated tile
 };
 
 #endif // TILESET_H
